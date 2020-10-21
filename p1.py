3ea6805bc223f2ee90dbbeb593be33c678a41e63
leemingxun <57944317+leemingxun@users.noreply.github.com>
2020-10-21 14:28:11 +0800

    Update p1.py

--- p1.py
+++ p1.py
@@ -3,18 +3,19 @@ import numpy as np
 
 
 def p1_has_cycle(sets):
-    # TODO
-    # return True if the graph has cycle; return False if not
-    '''
-      HINT: You can `print(sets)` to show what the matrix looks like
-        If we have a directed graph with 2->3 4->1 3->5 5->2 0->1
-               0  1  2  3  4  5
-            0  0  0 -1  1  0  0
-            1  0  1  0  0 -1  0
-            2  0  0  0 -1  0  1
-            3  0  0  1  0  0 -1
-            4 -1  1  0  0  0  0
-        The size of the matrix is (5,6)
-    '''
-    return False
+    A1=csr_matrix(sets)    
+    switch=False
+    while A1.get_shape()[0] > 1 and not switch:
+        is_one=np.where(A1[0].A.T == 1)[0][0]
+        is_negone=np.where(A1[0].A.T == -1)[0][0]
+        rewireable = np.where(A1.T[is_one].A.T== -1)[0]
+        for i in rewireable:
+            if np.logical_and((A1.T[is_one].A == -1)[0][i],(A1.T[is_negone].A == 1)[0][i]) :
+                switch=True
+                break
+            else:
+                p=A1[0]+A1[i]
+                A1=vstack([A1,p])
+        A1=A1[1:]
+    return switch
 
